3482af99f42cd6c9f1b9d41721c94fbec2ee6f5e
RyanNovas <ryan@quill.org>
2018-04-30 10:50:43 -0400

    adds auditor to segment user serializer

--- app/serializers/segment_analytics_user_serializer.rb
+++ app/serializers/segment_analytics_user_serializer.rb
@@ -1,4 +1,8 @@
 class SegmentAnalyticsUserSerializer < UserSerializer
-  attributes :email, :created_at
+  attributes :email, :created_at, :auditor
+
+  def auditor
+    current_user.flags.include?('auditor')
+  end
 
 end
