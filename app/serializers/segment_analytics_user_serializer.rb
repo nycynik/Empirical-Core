f26ba8ede73637cb83202d6c9bc35ab7e396bed1
RyanNovas <ryan@quill.org>
2018-04-30 11:34:23 -0400

    change current_user to object in serializer

--- app/serializers/segment_analytics_user_serializer.rb
+++ app/serializers/segment_analytics_user_serializer.rb
@@ -2,7 +2,7 @@ class SegmentAnalyticsUserSerializer < UserSerializer
   attributes :email, :created_at, :auditor
 
   def auditor
-    current_user.flags.include?('auditor')
+    object.flags.include?('auditor')
   end
 
 end
